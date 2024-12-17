ff6a00026c8af88ebb8dde6590d021866b979542
chasehaye <chasehayejoc@gmail.com>
2024-12-17 00:49:42 -0700

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center w-full mx-auto mt-10">
+        start profile development
+      </div>
+    </>
+  )
+}
+
+export default App
